65fb8bc9f8ec51d5979f28860518310a420e38ce
Simon <lx.simon@yahoo.com>
2017-09-07 14:08:22 +0800

    kuromoji japanese sample

---
+++ src/main/java/cn/analyzer/kuromoji/JapaneseAnalyzer.java
@@ -0,0 +1,16 @@
+package cn.analyzer.kuromoji;
+
+import com.atilika.kuromoji.ipadic.Token;
+import com.atilika.kuromoji.ipadic.Tokenizer;
+
+import java.util.List;
+
+public class JapaneseAnalyzer {
+    public static void main(String[] args) {
+        Tokenizer tokenizer = new Tokenizer();
+        List<Token> tokens = tokenizer.tokenize("お寿司が食べたい。");
+        for(Token token: tokens) {
+            System.out.println(token.getSurface() + "\t" + token.getAllFeatures());
+        }
+    }
+}
